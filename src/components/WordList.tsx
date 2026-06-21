187bb52e8922a350aeaa77b6b7acb0f15672111a
Your Name <you@example.com>
2026-06-21 14:44:40 +0800

    为了阅读例句方便，网站界面的单词列表两个单词在一行

--- src/components/WordList.tsx
+++ src/components/WordList.tsx
@@ -358,7 +358,7 @@ export default function WordList() {
                                     {/* 单词卡片网格 */}
                                     {isThirdExpanded && (
                                       <div className="p-4 bg-white">
-                                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4">
                                           {words.map((word: IELTSWord) => {
                                             const isFlipped = flippedCards.has(word.id);
                                             return (
